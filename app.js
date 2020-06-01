45883e38388a4609a086e92d33630e86a8586ddc
Marcos Lucas <marcosvaldeni@gmail.com>
2020-06-01 18:12:59 +0100

    Delete and filter tasks

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ loadEventListeners();
 function loadEventListeners() {
   // Add task event
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // Clear task event
+  clearBtn.addEventListener('click', clearTask);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -29,7 +35,7 @@ function addTask(e) {
   // Create new link element
   const link = document.createElement('a');
   // Add class
-  link.className = 'delete-item secodary-content';
+  link.className = 'delete-item secondary-content';
   // Add icon html
   link.innerHTML = '<i class="fa fa-remove"></i>';
   //Append the link to li
@@ -42,4 +48,36 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTask() {
+  // Faster
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  })
 }
